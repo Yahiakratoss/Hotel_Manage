bf8026a29b2270807a9b63f07f55953f59e2eaf8
Yahiakratoss <122739743+Yahiakratoss@users.noreply.github.com>
2024-12-18 13:59:14 +0100

    first change

---
+++ src/main/java/com/mycompany/logiciel_h/LOGICIEL_H.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logiciel_h;
+
+/**
+ *
+ * @author Yahia
+ */
+public class LOGICIEL_H {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
